7328f884d5c7a98ff674ede9352411e9340b9068
Alan Lee <aflee@wisc.edu>
2023-10-04 10:09:27 -0500

    Adding a Timer and updating HTML for Directory changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var timeclock = dayjs();
+var timeslot = document.getElementById("currentDay");
+
+
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+setInterval(function () {
+    timeclock = dayjs();
+    $(timeslot).text(timeclock.format('MMM D, YYYY [at] hh[:]mm[:]ss A '));
+    }, 1000);
+    console.log(timeslot);
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
